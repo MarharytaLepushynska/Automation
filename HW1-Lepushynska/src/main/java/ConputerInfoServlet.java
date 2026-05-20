2e1d652d72da43712750038795e0a97000a1b70a
MarharytaLepushynska <m.lepushynska@ukma.edu.ua>
2026-05-20 09:41:39 +0300

    add hometask1

---
+++ HW1-Lepushynska/src/main/java/ConputerInfoServlet.java
@@ -0,0 +1,37 @@
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+
+@WebServlet(name = "ComputerInfoServlet", value = "/computer-info-servlet")
+public class ConputerInfoServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        StringBuilder sb = new StringBuilder();
+        OperatingSystemMXBean osmxb = ManagementFactory.getOperatingSystemMXBean();
+        sb.append("<h1>" + "Info about computer where server is running" + "</h1>");
+        sb.append("<h3>" + "Name of OS: " + osmxb.getName()  + "</h3>");
+        sb.append("<h3>" +"Version: " + osmxb.getVersion()  + "</h3>");
+        sb.append("<h3>" +"Architecture: " + osmxb.getArch()  + "</h3>");
+        sb.append("<h3>" +"Available Processors: " + osmxb.getAvailableProcessors()  + "</h3>");
+        message = sb.toString();
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println(message);
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ HW1-Lepushynska/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Name</title>
+</head>
+<body>
+<h1><%= "Home work 1" %>
+</h1>
+<br/>
+<a href="my-surname-servlet">My surname Servlet</a>
+<br/>
+<a href="computer-info-servlet">Computer Info Servlet</a>
+</body>
+</html>
